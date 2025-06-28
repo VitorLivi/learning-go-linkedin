5b3bd2bcbc67b41387bd765171ace01605fe7e41
Vitor Livi <vitorlivi1999@gmail.com>
2025-06-28 11:23:45 -0300

    feat: :sparkles: learn how to output text

---
+++ output-text.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy dog"
+	aNumber := 42
+
+	fmt.Println(str1, str2, str3)
+	stringLength, err := fmt.Println("The value is", aNumber)
+
+	if err == nil {
+		fmt.Println("String length:", stringLength)
+	} else {
+		fmt.Println("Error:", err)
+	}
+
+	fmt.Printf("Value of number: %v\n", aNumber)
+	fmt.Printf("Data type: %T\n", aNumber)
+}
